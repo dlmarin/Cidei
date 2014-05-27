af3f1ea348402a4c3a7b4c34ddf245614745167d
Diana Lorena <diana238@gmail.com>
2014-05-27 18:06:17 -0500

    jquery y demás

--- Frontend/lab5/callbacks.js
+++ Frontend/lab5/callbacks.js
@@ -19,3 +19,30 @@ function otroCallback(miCallback){
 otroCallback(function(unValor){
 	console.log("Algo se imprime y es: " + unValor);
 });
+
+/* múltiples callbacks llamado en determinados casos*/
+
+function algoOcurre(callback1, callback2, callback3){
+	//pasa la primera vez
+	callback1("Primer paso");
+
+	//siguiente callback
+	callback2("Segundo paso");
+
+	//y por último
+	callback3("último y tercer paso");
+}
+
+//llamado y uso de los múltiples callbacks para "algoOcurre"
+
+algoOcurre(
+	function(paso1){
+		console.log(paso1);
+	},
+	function(paso2){
+		console.log(paso2);
+	},
+	function(paso3){
+		console.log(paso3);
+	}
+);
