45123ae28691c4be12cd31f5a1344714461b2c44
Diana Lorena <diana238@gmail.com>
2014-06-05 17:13:19 -0500

    lab11

--- Frontend/lab6/lab6-10/js/lab6-10.js
+++ Frontend/lab6/lab6-10/js/lab6-10.js
@@ -1,28 +1,45 @@
 $(document).ready(function(){
 	$('.error').hide();
 	$('.result').hide();
-	$('.errorRadio').hide();
+	$('.errorComida').hide();
+	$('.resultComida').hide();
+
 	
 	$('.button').bind('click', function(event){
 		var amount =0;
-		var count = $('input:checked').length;
+		var count = $('.checkbox:checked').length;
 		if (count===0) {
-			$('p.result').hide();
-			$('.error').show();
-			$('.error').text('Debe seleccionar un tipo de comida');
+			$('p.resultComida').hide();
+			$('.errorComida').show();
+			$('.errorComida').text('Debe seleccionar un tipo de comida');
 		}
 		else{
-			$('form').find(':checked').each(function(){
-				if($(this).is(':checked')){
+			$('form').find('.checkbox:checked').each(function(){
+				if($(this).is('.checkbox:checked')){
 					amount = amount + parseInt($(this).val());
 				}
 			});
-			$('.error').hide();
-			$('p.result').show();
-			$('p.result').text('La suma total es: ' + amount);
+			$('.errorComida').hide();
+			$('p.resultComida').show();
+			$('p.resultComida').text('La suma total es: ' + amount);
 		}
+		var data = $('.email').val();
+
+		if(valid_email(data)){
+			$('.error').hide();
+			$('.result').show();
+		}
+		else{
+			$('.error').show();
+			$('.result').hide();
+			$('.error').html('<h1>Ingrese una dirección de correo valida</h1>');
+		}	
 
 		event.preventDefault();
 
 	});
+	function valid_email(email){
+		var pattern = new RegExp(/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]+$/);
+		return pattern.test(email);
+	}	
 });
